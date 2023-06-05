6a798a3e822f06b8d57076984284de008b205346
Aditya Jawanjal <adityajawanjal01@gmail.com>
2023-06-05 21:21:57 +0530

    redux started.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,74 +1,20 @@
-import { Box, Button, Heading, Input, Stack, Text } from "@chakra-ui/react";
-import React, { useEffect, useRef, useState } from "react";
-import axios from "axios";
+import React from "react";
+import { Box, Heading, Input, Stack } from "@chakra-ui/react";
 
 const App = () => {
-  const [file, setFile] = useState();
-  const [url, setUrl] = useState();
-  const [fileName, setFileName] = useState();
-
-  const fileRef = useRef();
-
-  useEffect(() => {
-    file ? setFileName(file.name) : "";
-  }, [file]);
-
-  const handleChangeFile = (e) => {
-    setFile(e.target.files[0]);
-  };
-
-  const submit = async () => {
-    const data = new FormData();
-    data.append("name", file.name);
-    data.append("file", file);
-    const res = await axios.post(
-      `${import.meta.env.VITE_SERVER_URL}/api/uploads`,
-      data
-    );
-    setUrl(res.data);
-  };
-
   return (
     <>
-      <Stack
-        h={"99.5vh"}
-        bgColor={"blackAlpha.900"}
-        color={"whitesmoke"}
-        alignItems={"center"}
-        py={"20"}
-        px={"4"}
-        gap={"10"}
-      >
-        <Heading>Upload Media and Get Link to share !</Heading>
-        <Input
-          type="file"
-          onChange={(e) => handleChangeFile(e)}
-          ref={fileRef}
-          display={"none"}
-        />
-        <Box>
-          <Button
-            size={"lg"}
-            bgColor={"blue.300"}
-            onClick={() => {
-              fileRef.current.click();
-            }}
-            w={"60"}
-          >
-            {" "}
-            Upload File
-          </Button>
-          <Text mt={"2"}>Selected File : {fileName}</Text>
-        </Box>
-        <Button size={"lg"} onClick={submit} w={"60"}>
-          Submit
-        </Button>
-        <Box borderBottom={"2px solid blue"} pb={"2"} wordBreak={"break-word"}>
-          <a href={url} target="_blank" rel="noopener noreferrer">
-            {url}
-          </a>
-        </Box>
-      </Stack>
+      <Box bgColor={"black"} color={"whitesmoke"} minH={"full"} px={"4"} >
+        <Stack alignItems={"center"} gap={20} w={"full"} py={"20"}>
+          <Heading>Todo App - Add new Tasks !</Heading>
+          <Input
+            type="text"
+            placeholder="Type here...✍"
+            border={"2px solid blue"}
+            w={{base:"80",md:"96"}}
+          />
+        </Stack>
+      </Box>
     </>
   );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { ChakraProvider, theme } from "@chakra-ui/react"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-    <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </Provider>
+  </React.StrictMode>
+);
